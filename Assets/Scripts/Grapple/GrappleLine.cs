325f9d95c4ec9688a75ff54a970e497b8e89bae6
DominicBellisario <dlb5102@g.rit.edu>
2025-10-04 18:42:17 -0400

    grapple line is now a chain link
    it retracts and extends like you think it should

--- Assets/Scripts/Grapple/GrappleLine.cs
+++ Assets/Scripts/Grapple/GrappleLine.cs
@@ -1,8 +1,9 @@
-using System.Collections;
 using UnityEngine;
 
+[RequireComponent(typeof(LineRenderer))]
 public class GrappleLine : MonoBehaviour
 {
+    [Header("References")]
     [SerializeField] GameObject grappleMuzzle;
     [SerializeField] GameObject grappleHead;
 
@@ -12,10 +13,15 @@ public class GrappleLine : MonoBehaviour
     {
         line = GetComponent<LineRenderer>();
     }
-
     void Update()
     {
+        Vector3 start = grappleMuzzle.transform.position;
+        Vector3 end = grappleHead.transform.position;
+
         line.SetPosition(0, grappleMuzzle.transform.position);
         line.SetPosition(1, grappleHead.transform.position);
-    } 
+
+        float distance = Vector3.Distance(start, end);
+        line.material.mainTextureOffset = new Vector2(-distance * 2.0f, 0);
+    }
 }
